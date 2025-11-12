023737eb5f8aa6e90352fbc9692436d263b5d9e6
chandruarumugam777-star <chandruarumugam777@gmail.com>
2025-11-12 12:33:20 +0530

    Delete stdn mark.py

--- stdn mark.py
+++
@@ -1,9 +0,0 @@
-stdn=int(input("mark:"))
-if(stdn<=35):
-    print("the stdnt is poor")
-elif(stdn>=35 and stdn<=75):
-    print("the stdnt is average")
-elif(stdn>75 and stdn<100):
-    print("the stdn is good")
-else:
-    print("invalid input")
